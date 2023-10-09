86ba85a70b078a356322194e013d54bdae7a1fea
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-10-09 19:24:57 +0400

    index.ts configuration added

--- src/mail/index.ts
+++ src/mail/index.ts
@@ -5,6 +5,9 @@ dotenv.config();
 
 const gmailTransport = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
